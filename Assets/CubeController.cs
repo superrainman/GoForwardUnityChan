e210f014b437edf1245b3b584a9a4b20a2da3c74
奥山翔平 <amewotoko@gmail.com>
2020-07-13 17:13:09 +0900

    Lesson7 課題
    ブロックが「ユニティちゃん」タグを持つオブジェクト以外に接触すると効果音が鳴ります。

---
+++ Assets/CubeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour 
+{
+	private float speed = -12;
+	private float deadLine = -10;
+
+	//AudioSourceの取得。
+	private AudioSource sound;
+
+	// Use this for initialization
+	void Start () 
+	{
+		sound = GetComponent<AudioSource>();		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Translate (this.speed * Time.deltaTime, 0, 0);
+
+		if (transform.position.x <this.deadLine)
+		{
+			Destroy (gameObject);
+		}
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		//接触相手にunityちゃんタグが付いていない時、効果音を鳴らす。
+		if (other.gameObject.tag != "UnityChanTag")
+		{
+		sound.PlayOneShot(sound.clip);
+		}
+	}
+}
